a09041008c45b18e7da0184abc3e618bc3cf91ef
pravakarcode <pravakaradhikari13@gmail.com>
2025-06-25 00:49:37 +0545

    fixed

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: { unoptimized: true },
+  output: "export",
+  trailingSlash: true, // Important for GitHub Pages routing
 };
 
 module.exports = nextConfig;
